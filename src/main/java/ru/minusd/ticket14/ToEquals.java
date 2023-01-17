f753058343c4e5f767b5c529a359d42d274bfdf3
Дмитрий <50549999+MinusD@users.noreply.github.com>
2023-01-17 19:24:44 +0300

    fix 14

--- src/main/java/ru/minusd/ticket14/ToEquals.java
+++ src/main/java/ru/minusd/ticket14/ToEquals.java
@@ -4,19 +4,33 @@ package ru.minusd.ticket14;
  * Метод equals(), который принимамет 2 стека и возращает true, если они равны.
  */
 
+import java.util.Objects;
 import java.util.Stack;
 
 public class ToEquals {
     public static boolean equals(Stack<Integer> stack1, Stack<Integer> stack2) {
-        if (stack1.size() != stack2.size()) {
+        Stack<Integer> tmp = new Stack<>();
+        if (stack1.size() == stack2.size()) {
+            while (!stack1.isEmpty()) {
+                if (Objects.equals(stack1.peek(), stack2.peek())) {
+                    tmp.push(stack1.pop());
+                    stack2.pop();
+                } else {
+                    while (!tmp.isEmpty()) {
+                        stack1.push(tmp.peek());
+                        stack2.push(tmp.pop());
+                    }
+                    return false;
+                }
+            }
+            while (!tmp.isEmpty()) {
+                stack1.push(tmp.peek());
+                stack2.push(tmp.pop());
+            }
+            return true;
+        } else {
             return false;
         }
-        for (int i = 0; i < stack1.size(); i++) {
-            if (stack1.get(i) != stack2.get(i)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static void main(String[] args) {
