efbc8e517867939551f158021e541bf4a7d6cca6
Дмитрий <50549999+MinusD@users.noreply.github.com>
2023-01-17 13:13:09 +0300

    Добавлены билеты 9 Переделаны ссылки

---
+++ src/main/java/ru/minusd/ticket03/ForIsUnique.java
@@ -0,0 +1,32 @@
+package ru.minusd.ticket03;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ForIsUnique {
+    public static boolean isUnique(Map<String, String> map) {
+        for (String key : map.keySet()) {
+            for (String key2 : map.keySet()) {
+                if (!Objects.equals(key, key2) && map.get(key).equals(map.get(key2))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Marty", "Stepp");
+        map.put("Stuart", "Reges");
+        map.put("Jessica", "Miller");
+        map.put("Amanda", "Camp");
+        map.put("Hal", "Perkins");
+
+        System.out.println(isUnique(map));
+
+        map.put("Kendrick", "Perkins");
+        System.out.println(isUnique(map));
+    }
+}
